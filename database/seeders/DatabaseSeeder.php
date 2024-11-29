72778092cf4ed7900e80c0784896e79bc7a2a42e
Davidsoulix <133910692+Davidsoulix@users.noreply.github.com>
2024-11-29 13:35:38 -0400

    sin leccion

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);// llama al seeder usuario
         $this->call(SuscripcionSeeder::class);// llama al seeder suscripcion
         $this->call(NivelSeeder::class);// llama al seeder nivel
-        $this->call(LeccionSeeder::class);// llama al seeder leccion
 
     }
 }
